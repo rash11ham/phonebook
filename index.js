e417fed083311b931c48af34dc464908b80fc28c
RashedElam <rashedelham3@gmail.com>
2024-10-12 12:40:34 +1100

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 //exercise 3.9
 const cors = require('cors')
 app.use(cors())
+
+
+//exercise 3.13
+const Person = require('./models/person')
+
 
 //exercise 3.10
 app.use(express.static("dist"));
@@ -21,93 +27,97 @@ app.use(
 app.use(express.json())
 
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/', (req,res) => {
     res.send('<h1>Hello</h1>')
 })
 //exercise 3.1
-app.get('/api/persons', (req,res) => {
-    res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then(persons => {
+        if (persons) {
+            res.json(persons);
+        } else {
+            res.status(404).end()
+        }
+    }).catch (error => next(error))
+    
 })
 
 //exercise 3.2
-app.get('/info', (req, res) => {
-    res.send(`<h3>Phonebook has info for ${persons.length} people</h3>
-        <br/>
-        ${Date()}`)
-})
+// app.get('/info', (req, res) => {
+//     res.send(`<h3>Phonebook has info for ${persons.length} people</h3>
+//         <br/>
+//         ${Date()}`)
+// })
 
 //exercise 3.3
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(p => p.id === id)
-    if (!person) {
-        res.status(404).send('no such person exist')
-    } 
-    res.json(person)
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
+      res.json(person);
+    }).catch(error => next(error))
+    
+    
 })
 
 //exercise 3.4
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(p => p.id !== id)
-    
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-//exercise 3.5
-function generateId() {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => Number(p.id)))
-        : 0
-    return String(maxId+1)
-}
-app.post('/api/persons', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
     const body = req.body
-    if (!body.name || !body.number) {
-        res.status(400).json({
-            error: "Contents cannot be empty"
-        })
-    } 
-    const existingPerson = persons.find(p=> p.name === body.name)
-    if (existingPerson) {
-        res.status(409).json({
-            error: "Name exists please try another name"
-        })
-    } 
-    
     const person = {
-        id: generateId(),
         name: body.name,
         number: body.number
     }
-    persons = persons.concat(person)
-    res.json(person)
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatePerson => {
+            res.json(updatePerson)
+        }).catch(error => next(error))
 })
 
-app.listen(3001, () => {
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (body.content === undefined) {
+      return res.status(400).json({ error: "content missing" });
+    }
+
+    // Person.find({}).then(persons => {
+    //     persons.forEach(person => {
+    //         if (body.name === person.name) {
+    //             console.log(body.name)
+    //         }
+    //     })
+    // })
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then(newPerson => {
+        res.json(newPerson)
+    }).catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
     console.log('app is connected')
 })
 
